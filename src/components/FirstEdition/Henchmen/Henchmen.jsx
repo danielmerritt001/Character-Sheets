44f2536d2e26e4519e7dca611434b03ac467330a
Daniel Merritt <daniel.mafou@gmail.com>
2023-06-14 10:09:57 -0500

    henchman table build

--- src/components/FirstEdition/Henchmen/Henchmen.jsx
+++ src/components/FirstEdition/Henchmen/Henchmen.jsx
@@ -4,15 +4,49 @@ const Henchmen = () => {
     <>
       <div className="henchman">
         <h3>Henchmen/ Animal Companions</h3>
-        <div>name</div>
-        <div>race/ class</div>
-        <div>ac</div>
-        <div>HD/ lvl</div>
-        <div># Attacks</div>
-        <div>HP</div>
-        <div>THAC0/ or to hit chart</div>
-        <div>Damage</div>
-        <div>Abilities</div>
+        <table>
+          <tr>
+            <th>name</th>
+            <th>race/ class</th>
+            <th>ac</th>
+            <th>HD/ lvl</th>
+            <th># Attacks</th>
+            <th>HP</th>
+            <th>THAC0/ or to hit chart</th>
+            <th>Damage</th>
+            <th>Abilities</th>
+          </tr>
+          <tr>
+            <td>
+              <input type="text" />
+            </td>
+            <td>
+              <input type="text" />
+            </td>
+            <td>
+              <input type="text" />
+            </td>
+            <td>
+              <input type="text" />
+            </td>
+            <td>
+              <input type="text" />
+            </td>
+            <td>
+              <input type="text" />
+            </td>
+            <td>
+              <input type="text" />
+            </td>
+            <td>
+              <input type="text" />
+            </td>
+            <td>
+              <input type="text" />
+            </td>
+            
+          </tr>
+        </table>
       </div>
     </>
   );
