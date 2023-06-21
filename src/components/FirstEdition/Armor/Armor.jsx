26fa8a6b31223e9f685bbacff8ee5c7989e411e1
Daniel Merritt <daniel.mafou@gmail.com>
2023-06-21 09:49:19 -0400

    stub armor and shield card

--- src/components/FirstEdition/Armor/Armor.jsx
+++ src/components/FirstEdition/Armor/Armor.jsx
@@ -1,10 +1,36 @@
 const Armor = () => {
   return ( 
     <>
-    <div className="armor">
-        <h3>kind of armor/ name</h3>
+      <div className="armor">
+        <h3>Armor</h3>
+        <h4>Current AC</h4>
+        <input type="text" />
         <div>ac adjustment with without armor</div>
-        <div>shield?</div>
+        <input type="text" name="" id="" />
+      <table>
+        <tr>
+          <th>Name of Armor</th>
+          <th>Armor AC</th>
+          <th>Bonuses/Penalties</th>
+        </tr>
+        <tr>
+          <td><input type="text" /></td>
+          <td><input type="text" /></td>
+          <td><input type="text" /></td>
+        </tr>
+      </table>
+      <table>
+        <tr>
+          <th>Name of Shield</th>
+          <th>Shield AC adjustment</th>
+          <th>Bonuses/ Penalties</th>
+        </tr>
+        <tr>
+          <td><input type="text" /></td>
+          <td><input type="text" /></td>
+          <td><input type="text" /></td>
+        </tr>
+      </table>
       </div>
     </>
   );
