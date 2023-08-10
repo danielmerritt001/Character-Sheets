80629a22a8fcbf1d0d96efabe46f54836ba1b38d
Daniel Merritt <daniel.mafou@gmail.com>
2023-08-10 11:19:11 -0400

    basic css for weapons

--- src/components/FirstEdition/WeaponsCard/WeaponsCard.jsx
+++ src/components/FirstEdition/WeaponsCard/WeaponsCard.jsx
@@ -1,27 +1,31 @@
+import styles from "./Weapons.module.css"
+
 const WeaponsCard = () => {
   return (  
     <>
-      <div className="weapons">
+      <div className={`${styles.weapons}`}>
         <h3>weapons</h3>
       </div>
       <table>
         <tr>
-          <th>num of attacks</th>
-          <th>Size</th>
-          <th>speed</th>
-          <th>hit adjustment</th>
-          <th>damage adjustment</th>
-          <th>damage</th>
-          <th>range/special</th>
+          <th className={`${styles.name}`}>Name</th>
+          <th className={`${styles.attacks}`}># of att.</th>
+          <th className={`${styles.speed}`}>speed</th>
+          <th className={`${styles.toHit}`}>hit adj.</th>
+          <th className={`${styles.damage}`}>damage</th>
+          <th className={`${styles.size}`}>Size Adj</th>
+          <th className={`${styles.range}`}>range/length</th>
+          <th className={`${styles.special}`}>Special</th>
         </tr>
         <tr>
-          <td><input type="text" /></td>
-          <td><input type="text" /></td>
-          <td><input type="text" /></td>
-          <td><input type="text" /></td>
-          <td><input type="text" /></td>
-          <td><input type="text" /></td>
-          <td><input type="text" /></td>
+          <td><input className={`${styles.name}`} type="text" /></td>
+          <td><input className={`${styles.attacks}`} type="text" /></td>
+          <td><input className={`${styles.speed}`} type="text" /></td>
+          <td><input className={`${styles.toHit}`} type="text" /></td>
+          <td><input className={`${styles.damage}`} type="text" /></td>
+          <td><input className={`${styles.size}`} type="text" /></td>
+          <td><input className={`${styles.range}`} type="text" /></td>
+          <td><input className={`${styles.special}`} type="text" /></td>
         </tr>
       </table>
     </>
