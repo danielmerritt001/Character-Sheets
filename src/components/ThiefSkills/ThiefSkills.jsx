c024da317984326b6a1b3fa1f8a6db574ba4f29a
Daniel Merritt <daniel.mafou@gmail.com>
2023-08-11 09:42:12 -0400

    base thief skills css

--- src/components/ThiefSkills/ThiefSkills.jsx
+++ src/components/ThiefSkills/ThiefSkills.jsx
@@ -1,3 +1,5 @@
+import styles from "./ThiefSkills.module.css"
+
 const ThiefSkills = () => {
   return ( 
     <>
@@ -5,47 +7,47 @@ const ThiefSkills = () => {
     <table>
       <tr>
         <td></td>
-        <th>Pick Pockets</th>
-        <th>Open Locks</th>
-        <th>Find/ Rem Traps</th>
-        <th>Move Silently</th>
-        <th>Hide in Shadows</th>
-        <th>Hear Noise</th>
-        <th>Climb Walls</th>
-        <th>Read Languages</th>
+        <th className={`${styles.pocket}`}>Pick Pockets</th>
+        <th className={`${styles.lock}`}>Open Locks</th>
+        <th className={`${styles.trap}`}>Find/ Rem Traps</th>
+        <th className={`${styles.silent}`}>Move Silently</th>
+        <th className={`${styles.shadow}`}>Hide in Shadows</th>
+        <th className={`${styles.hear}`}>Hear Noise</th>
+        <th className={`${styles.climb}`}>Climb Walls</th>
+        <th className={`${styles.read}`}>Read Languages</th>
       </tr>
       <tr>
         <th>Base</th>
-        <td><input type="text" /></td>
-        <td><input type="text" /></td>
-        <td><input type="text" /></td>
-        <td><input type="text" /></td>
-        <td><input type="text" /></td>
-        <td><input type="text" /></td>
-        <td><input type="text" /></td>
-        <td><input type="text" /></td>
+        <td><input className={`${styles.pocket}`} type="text" /></td>
+        <td><input className={`${styles.lock}`} type="text" /></td>
+        <td><input className={`${styles.trap}`} type="text" /></td>
+        <td><input className={`${styles.silent}`} type="text" /></td>
+        <td><input className={`${styles.shadow}`} type="text" /></td>
+        <td><input className={`${styles.hear}`} type="text" /></td>
+        <td><input className={`${styles.climb}`} type="text" /></td>
+        <td><input className={`${styles.read}`} type="text" /></td>
       </tr>
       <tr>
         <th>Dex Adj</th>
-        <td><input type="text" /></td>
-        <td><input type="text" /></td>
-        <td><input type="text" /></td>
-        <td><input type="text" /></td>
-        <td><input type="text" /></td>
-        <td><input type="text" /></td>
-        <td><input type="text" /></td>
-        <td><input type="text" /></td>
+        <td><input className={`${styles.pocket}`} type="text" /></td>
+        <td><input className={`${styles.lock}`} type="text" /></td>
+        <td><input className={`${styles.trap}`} type="text" /></td>
+        <td><input className={`${styles.silent}`} type="text" /></td>
+        <td><input className={`${styles.shadow}`} type="text" /></td>
+        <td><input className={`${styles.hear}`} type="text" /></td>
+        <td><input className={`${styles.climb}`} type="text" /></td>
+        <td><input className={`${styles.read}`} type="text" /></td>
       </tr>
       <tr>
         <th>Total</th>
-        <td><input type="text" /></td>
-        <td><input type="text" /></td>
-        <td><input type="text" /></td>
-        <td><input type="text" /></td>
-        <td><input type="text" /></td>
-        <td><input type="text" /></td>
-        <td><input type="text" /></td>
-        <td><input type="text" /></td>
+        <td><input className={`${styles.pocket}`} type="text" /></td>
+        <td><input className={`${styles.lock}`} type="text" /></td>
+        <td><input className={`${styles.trap}`} type="text" /></td>
+        <td><input className={`${styles.silent}`} type="text" /></td>
+        <td><input className={`${styles.shadow}`} type="text" /></td>
+        <td><input className={`${styles.hear}`} type="text" /></td>
+        <td><input className={`${styles.climb}`} type="text" /></td>
+        <td><input className={`${styles.read}`} type="text" /></td>
       </tr>
     </table>
     </>
